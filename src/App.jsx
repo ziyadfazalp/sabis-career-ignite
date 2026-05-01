5cd7b4a1aaa07c784cd48891b5e3804e885bf80b
Ziyad Fazal <ziyadfazalp@gmail.com>
2026-05-01 20:10:11 +0530

    Fixed mobile page not opening at the top issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import React, { useEffect } from 'react'
 import Hero from './components/Hero'
 import EmpathyGrid from './components/EmpathyGrid'
 import Services from './components/Services'
 import About from './components/About'
-import Footer from './components/Footer' // <-- Add this import
+import Footer from './components/Footer'
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="min-h-screen bg-stone-50 font-sans text-gray-900">
       <Hero />
       <EmpathyGrid />
       <Services />
       <About />
-      <Footer /> {/* <-- Add the component here */}
+      <Footer />
     </div>
   )
 }
